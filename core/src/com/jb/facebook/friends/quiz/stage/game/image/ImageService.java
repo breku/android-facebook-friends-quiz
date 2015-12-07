9740bb99667f3ca3ae3bbd8f593bb9838646c577
breku <jakub.brek@gmail.com>
2015-12-07 22:35:04 +0100

    cache to load images

--- core/src/com/jb/facebook/friends/quiz/stage/game/image/ImageService.java
+++ core/src/com/jb/facebook/friends/quiz/stage/game/image/ImageService.java
@@ -3,11 +3,10 @@ package com.jb.facebook.friends.quiz.stage.game.image;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 /**
  * Created by brekol on 07.12.15.
@@ -16,16 +15,29 @@ public class ImageService {
 
     private static final String TAG = "ImageService";
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private Cache<String, TextureRegion> cache = CacheBuilder.newBuilder().maximumSize(200).expireAfterWrite(100, TimeUnit
+            .MINUTES).build();
 
-    public TextureRegion getImage(String url) {
-        final Future<ImageDetails> submit = executor.submit(new TextureRegionCallable(url));
+    public TextureRegion getImage(final String url) {
+
         try {
-            final ImageDetails imageDetails = submit.get();
-            return new TextureRegion(new Texture(imageDetails.getPixmap()), 0, 0, imageDetails.getWidth(), imageDetails.getHeight());
-        } catch (InterruptedException e) {
-            Gdx.app.log(TAG, "Error during downloading image", e);
+            return cache.get(url, new Callable<TextureRegion>() {
+                @Override
+                public TextureRegion call() throws Exception {
+                    final Future<ImageDetails> submit = executor.submit(new TextureRegionCallable(url));
+                    try {
+                        final ImageDetails imageDetails = submit.get();
+                        return new TextureRegion(new Texture(imageDetails.getPixmap()), 0, 0, imageDetails.getWidth(), imageDetails.getHeight());
+                    } catch (InterruptedException e) {
+                        Gdx.app.log(TAG, "Error during downloading image", e);
+                    } catch (ExecutionException e) {
+                        Gdx.app.log(TAG, "Error during downloading image", e);
+                    }
+                    return null;
+                }
+            });
         } catch (ExecutionException e) {
-            Gdx.app.log(TAG, "Error during downloading image", e);
+            Gdx.app.log(TAG, "Error during getting image from cache", e);
         }
         return null;
     }
